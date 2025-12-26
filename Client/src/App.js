e21a1bdda36614d0c9af5fd8f10d6e84ecddb663
vmangal11 <mvidisha06@gmail.com>
2025-12-26 15:53:07 +0530

    REDPLOY SITE

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,29 +1,25 @@
 // App.js
 
 import "./App.css";
-
 import HomePage from "./benevolent.jsx";
-
 import ContactForm from "./Contact.jsx";
-
 import BlogPage from "./Blogs.jsx";
+import ResetPassword from "./ResetPassword.jsx";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import ResetPassword from "./ResetPassword.jsx";
+import ScrollToTop from "./ScrollToTop"; // ✅ ADD THIS
 
 function App() {
   return (
     <Router>
-           {" "}
+      <ScrollToTop /> {/* ✅ ADD THIS */}
       <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/contact" element={<ContactForm />} />
-                <Route path="/blog" element={<BlogPage />} />
-                <Route path="/reset-password" element={<ResetPassword />} />
-                <Route path="*" element={<div>404 Not Found</div>} />     {" "}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
-         {" "}
     </Router>
   );
 }
